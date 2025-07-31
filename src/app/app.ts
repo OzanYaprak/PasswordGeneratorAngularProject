1addd2cd2398723afdc4fc4990ee682f3293f792
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2025-07-31 16:40:31 +0300

    firstCommit

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,32 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
 export class App {
   protected readonly title = signal('password');
+  
+  includeLetters:boolean = false;
+  includeNumbers:boolean = false;
+  includeSysmbols:boolean = false;
+
+  modifyLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  modifyNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+  modifySymbols(){
+    this.includeSysmbols = !this.includeSysmbols;
+  }
+
+  generatePassword(){
+    console.log
+    (`Değerlerim;
+      Alfabe: ${this.includeLetters},
+      Sayılar: ${this.includeNumbers},
+      Semboller: ${this.includeSysmbols}
+      `);
+  }
 }
