f23c6730b0b97ed440e24ab129733badf124f7a8
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2025-08-03 01:27:04 +0300

    Update Angular dependencies and improve password display logic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+// import { provideAngularFeatures } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    // provideAngularFeatures({
+    //   signal: true,
+    //   signalEffects: true,
+    //   signalForms: true,
+    //   signalRouter: true
+    // })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
